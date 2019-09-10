142d7dd8c335a5507e782f823faffc2813f50888
Kang HuaiShuai <khs1994@khs1994.com>
2019-09-10 20:57:43 +0800

    Update actions

--- README.md
+++ README.md
@@ -8,15 +8,15 @@
 
 # 目的
 
-在 Travis CI 使用 GitBook 构建文档，大部分时间用在了安装 GitBook 环境上，将环境打包到 Docker，大大节约了时间。
+在 CI 环境中使用 GitBook 构建文档，大部分时间用在了安装 GitBook 环境上，将环境打包到 Docker，大大节约了时间。
 
 # Overview
 
 * [`Node.js`](https://github.com/docker-library/docs/tree/master/node) `GitBook` 均为最新版
 
-* 本镜像主要完成 `GitBook` 及其插件的安装，具体请查看 `book.json` 文件
+* 本镜像主要完成 `GitBook` 及其插件的安装，具体请查看 `alpine/book.json` 文件
 
-* 基于 `book.json` 可以构建自己的镜像
+* 基于 `alpine/book.json` 可以构建自己的镜像
 
 # Usage
 
@@ -48,19 +48,53 @@ $ docker run -it --rm \
     -v ~/.ssh:/root/.ssh \
     -e GIT_USERNAME=username \
     -e GIT_USEREMAIL=username@domain.com \
-    -e GIT_REPO=git@github.com:username/repo.git \
+    -e GIT_REPO=git@github.com:username/repo \
     -e GIT_BRANCH=master \
     khs1994/gitbook \
     deploy
 ```
 
-## Actions
-
-* https://developer.github.com/actions/
+有两种验证方式，第一种像上面一样挂载 ssh 文件，使用 `ssh` 方式验证，第二种方式你可以通过 `TOKEN` 验证。
 
 ```bash
-
+$ docker run -it --rm \
+    -v $PWD:/srv/gitbook-src \
+    -e GIT_USERNAME=username \
+    -e GIT_USEREMAIL=username@domain.com \
+    -e GIT_TOKEN=mytoken \
+    -e GIT_REPO=github.com/username/repo \
+    -e GIT_BRANCH=master \
+    khs1994/gitbook \
+    deploy
 ```
+
+## [GitHub Actions](https://help.github.com/en/categories/automating-your-workflow-with-github-actions)
+
+```yaml
+- name: GitBook Build
+  uses: docker://khs1994/gitbook
+- name: GitBook Deploy
+  uses: docker://khs1994/gitbook
+  if: github.event_name == 'push'
+  with:
+    args: deploy
+  env:
+    GIT_USERNAME: "khs1994"
+    GIT_USEREMAIL: "khs1994@khs1994.com"
+    GIT_BRANCH: "gh-pages"
+    GITHUB_TOKEN: ${{ secrets.PCIT_GIT_TOKEN }}
+    # 如果使用默认的 ${{ secrets.GITHUB_TOKEN }} 推送之后，pages 服务不会自动构建
+    # 造成 pages 服务不能使用
+    # 故请使用自己的 Token，请自行到 GitHub 页面生成 Token
+
+    # GIT_REPO: github.com/username/repo
+    # 这个变量在 GitHub Actions 中自动识别，无需传入。
+    # 如果你需要将 gitbook 生成的页面推送到别的仓库，你可以传入该变量
+    # GIT_TOKEN:
+    # 传入了 GIT_REPO 变量，必须传入 GIT_TOKEN 变量
+```
+
+* More Examples: https://github.com/khs1994-website/composer-docs.zh-cn/blob/master/.github/workflows/gitbook.yaml
 
 # Who use this image ?
 
--- alpine/docker-entrypoint.sh
+++ alpine/docker-entrypoint.sh
@@ -7,11 +7,10 @@ START=`date "+%F %T"`
 if [ $1 = "sh" ];then sh ; exit 0 ; fi
 if [ $1 = "version" ];then exec gitbook --version ; fi
 
-git config --global user.name ${GIT_USERNAME:-none}
+git config --global user.name ${GIT_USERNAME:-CI}
+git config --global user.email ${GIT_USEREMAIL:-ci@khs1994.com}
 
-git config --global user.email ${GIT_USEREMAIL:-none@none.com}
-
-rm -rf node_modules _book
+rm -rf node_modules
 
 WORKDIR=$PWD
 
@@ -19,37 +18,61 @@ cp -a . /srv/gitbook
 
 cd /srv/gitbook
 
+print_info(){
+  set +x
+  echo ""
+  echo "==> $@"
+  echo ""
+  set -x
+}
+
 main(){
-  gitbook build || ( gitbook install ; gitbook build )
+  gitbook build || (print_info "Build error, retry" ; gitbook install && gitbook build )
 
   cp -a _book $WORKDIR
+
   case $1 in
     server )
       exec gitbook serve
-      exit 0
       ;;
     deploy )
       set +x
 
-      echo ""
-      echo "==> check github actions ..."
-      echo ""
-
+      if [ -z "$GIT_REPO" ];then
+      # check github actions
       test -n "$GITHUB_ACTION" && \
-      GIT_REPO="https://${GIT_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-      set -x
+      print_info "RUN on GitHub Actions" && set +x && \
+      GIT_REPO="https://${GIT_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}"
+      fi
 
+      if [ -z "$GIT_REPO" ];then
+        print_info "miss \$GIT_REPO"
+        exit 1
+      fi
+
+      test -n "$GIT_TOKEN" && \
+      print_info "Auth by token" && set +x && \
+      GIT_REPO="https://${GIT_USERNAME}:${GIT_TOKEN}@${GIT_REPO}"
+
+      set -x
       cd _book || exit 1
-      git init
-      git remote add origin ${GIT_REPO}
+
+      if [ -d .git ];then
+        git remote set-url origin ${GIT_REPO}
+      else
+        rm -rf .git
+        git init
+        git remote add origin ${GIT_REPO}
+      fi
+
       git add .
       COMMIT_DATE=`date "+%F %T"`
-      git commit -m "${GIT_COMMIT_MESSAGE:-Gitbook updated:} ${COMMIT_DATE}"
+      git commit -m "${GIT_COMMIT_MESSAGE:-Gitbook updated:} ${COMMIT_DATE}" -s
       git push -f origin master:${GIT_BRANCH:-gh-pages}
       ;;
     esac
-    echo $START
-    date "+%F %T"
+    print_info $START
+    print_info $(date "+%F %T")
 }
 
 main $@
